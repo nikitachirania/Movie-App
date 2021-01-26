c1db59687774b7f8e17fd1ef10033259d2b7c590
nikitachirania <nikitachirania15@gmail.com>
2021-01-26 16:06:32 +0530

    v2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 var request = require("request");
 
 app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 app.get("/", function(req, res){
    res.render("search");
@@ -16,16 +17,18 @@ app.post("/addresults", function(req, res){
    // in get method we can get data from query
      var query = req.body.search;
     var url = "http://www.omdbapi.com/?t="+query+"&apikey=df9c69f3";
-    console.log(url);
     request(url, function(error, response,body){
-       //response.statusCode (success code=404 is not found that url)
-        if(!error && response.statusCode == 200) {
-             var data = JSON.parse(body);
-             console.log(data);
-            res.render("results",{data:data});
+        if(error){
+            res.render('results', {data: null, error: 'Error, please try again'});
+          }
+        else{
+            var data = JSON.parse(body);
+            if(data.Response=="True") {
+               res.render("results",{data:data, error: null});
+             }
+           else 
+                res.render('results', {data: null, error: 'Error, please try again'});
         }
-        else 
-         console.log("error");
     });
 });
 
